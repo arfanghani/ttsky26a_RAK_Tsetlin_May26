77e460c660b8347860db20abca27d2d90dd1482f
ag <76513830+arfanghani@users.noreply.github.com>
2026-05-10 11:52:03 +0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,78 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    reg clk;
+    reg rst_n;
+    reg ena;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    reg [7:0] ui_in;
+    reg [7:0] uio_in;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+    tt_um_arfanghani_tsetlin_top dut (
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    always #5 clk = ~clk;
+
+    task shift_feature;
+        input bit v;
+        begin
+            ui_in[0] = v;
+            ui_in[1] = 1;
+            #10;
+        end
+    endtask
+
+    integer i;
+
+    initial begin
+
+        $dumpfile("tb.vcd");
+
+        $dumpvars(0, tb);
+
+        clk = 0;
+        rst_n = 0;
+        ena = 1;
+
+        ui_in = 0;
+        uio_in = 0;
+
+        #20;
+        rst_n = 1;
+
+        // Load feature vector
+        for (i = 0; i < 32; i = i + 1) begin
+
+            if (i < 12)
+                shift_feature(1'b1);
+            else
+                shift_feature(1'b0);
+        end
+
+        ui_in[1] = 0;
+
+        #20;
+
+        // Start inference
+        ui_in[2] = 1;
+
+        #50;
+
+        $finish;
+    end
 
 endmodule
